9a33ae787a631a5626b8422d1fb5be463a4e50be
Sybren Dijkstra <nerbiz@gmail.com>
2018-04-20 15:17:53 +0200

    Simplified the migration creation + added the 'update' stub

--- src/Migrations/MigrationCreator.php
+++ src/Migrations/MigrationCreator.php
@@ -9,21 +9,8 @@ class MigrationCreator extends BaseMigrationCreator
     /**
      * {@inheritdoc}
      */
-    protected function getStub($table, $create)
+    public function stubPath()
     {
-        // Use a custom 'blank' stub
-        if ($table === null) {
-            return $this->files->get(dirname(__FILE__, 3) . '/stubs/migration-blank.stub');
-        }
-
-        // Use a custom 'create' stub
-        else if ($table !== null && $create) {
-            return $this->files->get(dirname(__FILE__, 3) . '/stubs/migration-create.stub');
-        }
-
-        // Otherwise fall back to the default stub
-        else {
-            return parent::getStub($table, $create);
-        }
+        return dirname(__FILE__, 3) . '/stubs/migrations';
     }
 }
