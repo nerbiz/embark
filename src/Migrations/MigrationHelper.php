8ace81789ad996f6d640447cc5be7a7af1948984
Sybren Dijkstra <nerbiz@gmail.com>
2018-05-03 10:02:17 +0200

    Dropped the 'primary/foreign key string' parts

--- src/Migrations/MigrationHelper.php
+++ src/Migrations/MigrationHelper.php
@@ -7,38 +7,6 @@ use Illuminate\Database\Schema\Blueprint;
 class MigrationHelper
 {
     /**
-     * Add a string column as the primary key to a table
-     * @param  Blueprint $table      The table to add the primary key to
-     * @param  string    $columnName The name of the primary key column
-     * @return void
-     */
-    public function primaryString(Blueprint $table, $columnName = 'id')
-    {
-        $table->string($columnName, config('embark.string_pk_length'));
-        $table->primary($columnName);
-    }
-
-    /**
-     * @see foreign()
-     * @return \Illuminate\Support\Fluent To enable chaining, for nullable(), after() etc.
-     */
-    public function foreignString(
-        Blueprint $table,
-        $foreignKey,
-        $foreignTable = null,
-        $onUpdate = null,
-        $onDelete = null
-    ) {
-        // Add the column
-        $column = $table->string($foreignKey, config('embark.string_pk_length'));
-
-        // Add the foreign key
-        $this->foreign($table, $foreignKey, $foreignTable, $onUpdate, $onDelete);
-
-        return $column;
-    }
-
-    /**
      * Shorter way of adding a foreign key
      * Assumes the format that 'category_id' references 'id' on 'categories'
      * @param  Blueprint   $table        The table to add the foreign key to
