42f8f61487a5c7cae522c8d67f10400850836a63
Sybren Dijkstra <nerbiz@gmail.com>
2019-04-07 18:15:18 +0200

    Added MySQL date/time formats to the database Helper

--- src/Database/Helper.php
+++ src/Database/Helper.php
@@ -7,6 +7,18 @@ use Illuminate\Support\Facades\Schema;
 class Helper
 {
     /**
+     * The MySQL date format
+     * @var string
+     */
+    const MYSQL_DATE_FORMAT = 'Y-m-d';
+
+    /**
+     * The MySQL datetime format
+     * @var string
+     */
+    const MYSQL_DATETIME_FORMAT = 'Y-m-d H:i:s';
+
+    /**
      * Apply the varchar length fix
      * @see https://laravel.com/docs/5.7/migrations#indexes
      * @return void
